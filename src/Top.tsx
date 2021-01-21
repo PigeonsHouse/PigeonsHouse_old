0f688ea469f3fbdcc364f3fb2d8bd608c9466c1a
PigeonsHouse <bullbull1230@gmail.com>
2021-01-22 04:23:09 +0900

    [ADD] コンテンツの作成

--- src/Top.tsx
+++ src/Top.tsx
@@ -6,26 +6,66 @@ import type { ContentProps } from './ContentProps';
 
 class Top extends React.Component{
   private contentgroup: ContentProps[]=[
-  {
-    classification: {
-      title: "うんち！www",
+    {classification: {
+      title: "Magic Adventure(共同制作)",
       contentText: (
         <div>
-          <p>うんちんちん</p>
+          <h2>GENRE: GAME</h2>
+          <p>
+            初めてきちんと完成させたゲーム．
+          </p>
+          作品URL: <a href='https://unityroom.com/games/magic_action_game_c3'>Magic Adventure</a>
         </div>
       )
-    }
-  },
-  {
-    classification: {
-      title: "unchi! lol",
+    }},
+    {classification: {
+      title: "水嵩 ﾐｽﾞｶｻ",
       contentText: (
         <div>
-          <p>poopenis</p>
+          <h2>GENRE: GAME</h2>
+          <p>
+            UnityRoom主催の1WeekGameJamでの作品．初めて一人で作ったゲーム．
+          </p>
+          作品URL: <a href='https://unityroom.com/games/vow_game_pigeon-house'>Mizukasa</a>
         </div>
       )
-    }
-  },
+    }},
+    {classification: {
+      title: "ロックマン.EXE Bot",
+      contentText: (
+        <div>
+          <h2>GENRE: WEB</h2>
+          <p>
+            C3部内のSNS，Mastodon内で稼働しているBot．C3部員の生活に役立ったり立たなかったりする機能が搭載している．
+          </p>
+          作品URL: <a href='https://mastodon.compositecomputer.club/@Rockmanexe'>RockmanEXEBot</a>
+        </div>
+      )
+    }},
+    {classification: {
+      title: "Trelloちゃん Bot",
+      contentText: (
+        <div>
+          <h2>GENRE: WEB</h2>
+          <p>
+            C3部内のSNS，Mastodon内で稼働しているBot．部内で利用しているイベント管理アプリ「Trello」と連携して連絡してくれる真面目な方のBot．
+          </p>
+          作品URL: <a href='https://mastodon.compositecomputer.club/@trello'>TrelloChanBot</a>
+        </div>
+      )
+    }},
+    {classification: {
+      title: "0.0046%の確率でポケモンを捕まえるサトシ",
+      contentText: (
+        <div>
+          <h2>GENRE: WEB</h2>
+          <p>
+            C3部内のSNS，Mastodon内で稼働しているBot．Botの名前の通り．約2.5ヶ月に1度ポケモンをゲットする確率になっています．
+          </p>
+          作品URL: <a href='https://mastodon.compositecomputer.club/@pokeget'>SatoshiBot</a>
+        </div>
+      )
+    }},
   ]
   render(){
     return (
@@ -33,10 +73,17 @@ class Top extends React.Component{
         <Header />
         <body className='contentBody'>
           <p>
-            このサイトは鳩屋敷が運営するホームページです．
+            このサイトは鳩屋敷が運営するホームページもといポートフォリオです．<br />
+            <a href='https://twitter.com/PigeonsHouse'>Twitter</a> <br />
+            <a href='https://mastodon.compositecomputer.club/@Pigeon_house2020'>Mastodon</a>
           </p>
-          <Content classification={this.contentgroup[0].classification}/>
-          <Content classification={this.contentgroup[1].classification}/>
+          {
+            this.contentgroup.map((fc, idx) => {
+              return (
+                <Content key={idx.toString()} classification={fc.classification} />
+              )
+            })
+          }
         </body>
       </div>
     )
